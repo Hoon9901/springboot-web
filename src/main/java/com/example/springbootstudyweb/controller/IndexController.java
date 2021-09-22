4dd0886bf787680b0799df913f4b9098b6c9035e
Hooooooon <shonn2323@gmail.com>
2021-09-22 22:19:11 +0900

    구글 로그인 연동 뷰 추가

--- src/main/java/com/example/springbootstudyweb/controller/IndexController.java
+++ src/main/java/com/example/springbootstudyweb/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springbootstudyweb.controller;
 
+import com.example.springbootstudyweb.config.auth.dto.SessionUser;
 import com.example.springbootstudyweb.dto.PostsResponseDto;
 import com.example.springbootstudyweb.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostService postService;
+    private final HttpSession httpSession;
 
     @GetMapping
     public String index(Model model) {  // Server Template에서 사용할 객체를 지정
         model.addAttribute("posts", postService.findAllDesc()); // service로 받은 객체를 posts로 index.must ... 에 전달
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }

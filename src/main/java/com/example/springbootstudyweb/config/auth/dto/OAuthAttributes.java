4bf56e14bab071b2094ceaa3e3f5e3591d88cc43
Hooooooon <shonn2323@gmail.com>
2021-09-25 20:54:54 +0900

    네이버 로그인 추가

--- src/main/java/com/example/springbootstudyweb/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/springbootstudyweb/config/auth/dto/OAuthAttributes.java
@@ -27,9 +27,12 @@ public class OAuthAttributes {
         this.picture = picture;
     }
     // OAuth2User에서 반환하는 정보는 Map임
-    public static OAuthAttributes of(String registrationId,
+    public static OAuthAttributes of(String registrationId,  // OAuth 제공자
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -40,6 +43,19 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName,
+                                           Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response"); // Json 필드 값 가져온다.
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
